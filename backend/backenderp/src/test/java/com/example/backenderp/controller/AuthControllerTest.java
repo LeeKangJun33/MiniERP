bee31dc297a24a8f1f3a08bdfff5465aa4f353ce
LeeKangJun33 <drfting1230@naver.com>
2025-04-22 17:36:56 +0900

    통합테스트 시도중

---
+++ backend/backenderp/src/test/java/com/example/backenderp/controller/AuthControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.backenderp.controller;
+
+import com.example.backenderp.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void signupTest() throws Exception {
+
+    }
+}
